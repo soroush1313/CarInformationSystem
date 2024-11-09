d58715c1fdf0dc21d4056ac8000cecc200059008
soroush.jj00 <soroush.jj00@gmail.com>
2024-11-09 16:56:20 +0100

    Implement JWTConfig

--- AuthService.API/Configurations/JwtConfig.cs
+++ AuthService.API/Configurations/JwtConfig.cs
@@ -2,5 +2,9 @@
 {
     public class JWTConfig
     {
+        public string Secret { get; set; }
+        public int ExpiryInMinutes { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
     }
 }
